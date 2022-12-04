2fcc8fc014f1e1571bc3066df5a23a38a74287ac
Repooc <tjcooper1982@gmail.com>
2022-12-04 15:42:19 -0500

    pass one of clean up (just formatting the file to what i am use to)

--- ElvUI_Zarac_G13/locales.lua
+++ ElvUI_Zarac_G13/locales.lua
@@ -72,22 +72,22 @@ if L then
   L['DESCRIPTION'] = [=[
   Este plugin posiciona os botões de suas Barra de Ação para ficarem igual o leiaute do Logitech G13 Gameboard.
   Ele usa a |cFFE6CC80barra 1|r e a |cFFE6CC80barra 2|r do ElvUI. Algumas opções destas barras foram desabilitadas.
-  
-  
-  
+
+
+
   De cima para baixo:
   A primeira linha usa os botões de #1 a #7 da |cFFE6CC80barra 2|r;
   A segunda linha usa os botões de #1 a #7 da |cFFE6CC80barra 1|r;
   A terceira linha usa os botões de #8 a #12 da |cFFE6CC80barra 1|r;
   A quarta linha usa os botões de #8 a #10 da |cFFE6CC80barra 2|r;
   Os botões #11 e #12 da |cFFE6CC80barra 2|r não são usados, e por isso, ocultos.
-  
+
   Se você deseja um fundo para as barras, marque a opção nas configurações da |cFFE6CC80barra 2|r.
- 
-  Este addon não armazena suas configurações anteriores. Se você o remover deverá usar o botão Restaurar Barra nas configurações das barras 1 e 2.    
+
+  Este addon não armazena suas configurações anteriores. Se você o remover deverá usar o botão Restaurar Barra nas configurações das barras 1 e 2.
   ]=]
   L['Description'] = 'Descrição'
-  L['Authors'] = 'Autores'  
+  L['Authors'] = 'Autores'
 end
 
 --Russian Localizations
